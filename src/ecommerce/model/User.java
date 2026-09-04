4fb7b88f73fafb94513fcce691548fcfcadd6473
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-09-04 17:19:34 +0530

    User

---
+++ src/ecommerce/model/User.java
@@ -0,0 +1,32 @@
+package ecommerce.model;
+
+public class User {
+
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
